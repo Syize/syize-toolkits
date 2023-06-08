c299c85636a59073d669cd6b7a98eeb22c583e50
Syize <syizeliu@gmail.com>
2023-06-08 19:43:05 +0800

    add function to convert pdf to picture; update version to 1.2.0

--- syize/picture_run.py
+++ syize/picture_run.py
@@ -2,6 +2,7 @@ from syize.picture import *
 from syize.string import *
 from getopt import getopt, GetoptError
 from sys import argv
+from typing import Union
 
 from syize.utils import to_file
 
@@ -16,20 +17,27 @@ def print_help(option: str = None):
           "\t-o [filename] | --output [filename]    output filename, default is stdout\n\n"
           "Function options\n"
           "\t--ocr                                  extract text from picture\n"
-          "\t--ocr-text                             type of text, default is en\n")
+          "\t--ocr-text                             type of text, default is en\n"
+          "\t--pdf                                  convert pdf to picture\n"
+          "\t--pdf-start [number]                   start page number\n"
+          "\t--pdf-end [number]                     end page number\n"
+          "\t--pdf-dpi [number]                     picture dpi\n")
     exit(0)
 
 
-def get_options() -> dict[str, str]:
+def get_options() -> dict[str, Union[str, int, None]]:
     result = {
         'input': None,
         'output': None,
         'func': None,
-        'ocr-text': 'en'
+        'ocr-text': 'en',
+        'pdf-start': None,
+        'pdf-end': None,
+        'pdf-dpi': 300
     }
 
     try:
-        options, _ = getopt(argv[1:], "hi:o:", ["help", "input=", "output=", "ocr", "ocr-text="])
+        options, _ = getopt(argv[1:], "hi:o:", ["help", "input=", "output=", "ocr", "ocr-text=", "pdf", "pdf-start=", "pdf-end=", "pdf-dpi="])
         for key, value in options:
             if key in ["-h", "--help"]:
                 print_help()
@@ -41,6 +49,14 @@ def get_options() -> dict[str, str]:
                 result['func'] = "ocr"
             elif key in ["--ocr-text"]:
                 result['ocr-text'] = value
+            elif key in ["--pdf"]:
+                result['func'] = "pdf"
+            elif key in ["--pdf-start"]:
+                result['pdf-start'] = int(value)
+            elif key in ["--pdf-end"]:
+                result['pdf-end'] = int(value)
+            elif key in ["--pdf-dpi"]:
+                result['pdf-dpi'] = int(value)
             else:
                 print_help(key)
     except GetoptError:
@@ -60,6 +76,8 @@ def run():
         res = picture_to_string(options['input'], options['ocr-text'])
         res = remove_redundant_linebreak(res)
         to_file(res, options['output'])
+    elif options['func'] == "pdf":
+        pdf_to_picture(options['input'], folder_path=options['output'], start=options['pdf-start'], end=options['pdf-end'], dpi=options['pdf-dpi'])
     else:
         raise Exception(f"Unknown option {options['func']}")
 
